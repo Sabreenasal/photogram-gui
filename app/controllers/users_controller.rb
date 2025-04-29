5b9069e0690c4032a7ef28463bac67b2dbbe8131
Sabreena Salomon <salomonbree@gmail.com>
2025-04-29 03:53:32 +0000

    added routes to user/routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,29 @@ end
 def show 
   parameter = params.fetch("path_username")
 
-  @the_user = User.where({ :username => parameter }).at(0)
+  @user = User.where({ :username => parameter }).at(0)
   
   render({ :template => "user_templates/show"})
 
 end
 
-def create
+def add_user
+  input_username = params.fetch("query_username")
 
+  add_user = User.new
+  add_user.username = input_username
+  add_user.save
+
+  redirect_to("/users/#{input_username}")
+end
+def update
+  id = params.fetch("path_id")
+  input_username = params.fetch("query_username")
+
+  update_username = User.where({ :id => id }).at(0)
+  update_username.username = input_username
+  update_username.save
+
+  redirect_to("/users/#{input_username}")
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  
+  get("/", { :controller => "users", :action => "index"})
+
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show"})
+  post("/add_user", { :controller => "users", :action => "add_user" })
+  post("/update_user/:path_id", { :controller => "users", :action => "update" })
   
 end
